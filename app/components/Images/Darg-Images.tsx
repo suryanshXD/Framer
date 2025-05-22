6481b46c6d41cd3634b513a95b3790334a6c454d
Suryansh <suryanshvaish1@gmail.com>
2025-05-22 20:08:54 +0530

    img comp add

---
+++ app/components/Images/Darg-Images.tsx
@@ -0,0 +1,29 @@
+import { log } from "console";
+import { motion } from "framer-motion";
+
+interface style {
+  width: number;
+  img: any;
+}
+
+export default function DragImage({ img, width }: style) {
+  return (
+    <>
+      <div className="border border-black p-1.5 rounded-lg ">
+        <motion.img
+          drag
+          dragConstraints={{
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+          src={`${img}`}
+          alt="img"
+          width={`${width}`}
+          className="rounded-lg relative"
+        />
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,25 @@ import React from "react";
 import RainbowButton from "./components/Rainbow-Button";
 import AnimatedButton from "./components/Animated-Button";
 import DragableButton from "./components/Dragable-Button";
+import DragImage from "./components/Images/Darg-Images";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col justify-evenly items-center h-screen bg-neutral-200">
+      <div className="flex flex-col justify-evenly items-center h-screen ">
         <div>
           <RainbowButton text="Get Started" />
         </div>
         <AnimatedButton theme="black" textColor="white" text="Get Started" />
 
         <DragableButton theme="black" textcolor="white" text="Drag Button" />
+
+        <DragImage
+          img={
+            "https://images.unsplash.com/photo-1607992922515-7e38329e65d4?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww"
+          }
+          width={200}
+        />
       </div>
     </>
   );
