b80bf0b80f6fbd94b194803caaef66a8c89ae2d2
Suryansh <suryanshvaish1@gmail.com>
2025-05-23 22:37:02 +0530

    ThreeCardScroll added

---
+++ app/components/card/ThreeCardScroll.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { inView, motion } from "framer-motion";
+
+interface style {
+  img1: any;
+  img2: any;
+  img3: any;
+  width: number;
+}
+
+export default function ThreeCardScroll({ img1, img2, img3, width }: style) {
+  return (
+    <motion.div
+      initial="initial"
+      whileInView="inView"
+      viewport={{ amount: "all", once: true }}
+      className="flex justify-center size-"
+    >
+      <motion.img
+        className={`absolute bg-green-200 rounded-md z-20 text-center flex items-center justify-center`}
+        variants={{
+          initial: { x: 0, rotate: 0 },
+          inView: { x: -360, rotate: 0 },
+        }}
+        transition={{ duration: 2 }}
+        src={`${img1}`}
+        width={width}
+      />
+
+      <motion.img
+        className={`absolute bg-pink-200 rounded-md z-10 text-center flex items-center justify-center`}
+        variants={{
+          initial: { x: 0, rotate: 20 },
+          inView: { x: 0, rotate: 0 },
+        }}
+        transition={{ duration: 2 }}
+        src={`${img2}`}
+        width={width}
+      />
+
+      <motion.img
+        className={`absolute bg-blue-200 rounded-md z-20 text-center flex items-center justify-center`}
+        variants={{
+          initial: { x: 0, rotate: 10 },
+          inView: { x: 360, rotate: 0 },
+        }}
+        transition={{ duration: 2 }}
+        src={`${img3}`}
+        width={width}
+      />
+    </motion.div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 import React from "react";
-import RainbowButton from "./components/Rainbow-Button";
-import AnimatedButton from "./components/Animated-Button";
-import DragableButton from "./components/Dragable-Button";
-import DragImage from "./components/Images/Darg-Images";
-import HoverTiltImage from "./components/Images/Hover-Tilt-Image";
-import ThreeCardHover from "./components/card/ThreeCardHover";
+import ThreeCardScroll from "./components/card/ThreeCardScroll";
 
 const img1 =
   "https://img.freepik.com/free-photo/anime-japanese-character_23-2151478190.jpg?ga=GA1.1.1804695545.1747980337&semt=ais_hybrid&w=740";
@@ -24,7 +19,9 @@ export default function Home() {
   return (
     <>
       <div>
-        <ThreeCardHover img1={img1} img2={img2} img3={img3} size={60} />
+        <div className="mt-200 mb-500">
+          <ThreeCardScroll img1={img1} img2={img2} img3={img3} width={240} />
+        </div>
       </div>
     </>
   );
