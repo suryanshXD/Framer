ad2cba6b82cafebcdbcb73d4bf085fb0fc3c6c64
Suryansh <suryanshvaish1@gmail.com>
2025-05-24 17:39:18 +0530

    Tilt-Hover-Effect added

---
+++ app/components/3D Effect/Hover-Tilt-Image.tsx
@@ -0,0 +1,63 @@
+import { log } from "console";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
+
+interface style {
+  img: any;
+  width: number;
+}
+
+export default function HoverTiltImage({ img, width }: style) {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const mouseXspring = useSpring(x);
+  const mouseYspring = useSpring(y);
+
+  const rotateX = useTransform(mouseYspring, [-0.5, 0.5], ["18deg", "-18deg"]);
+  const rotateY = useTransform(mouseXspring, [-0.5, 0.5], ["18deg", "-18deg"]);
+
+  const handelMouseMove = (e: any) => {
+    const rect = e.target.getBoundingClientRect();
+
+    const height = rect.height;
+    const width = rect.width;
+
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    const xPert = mouseX / width - 0.5;
+    const yPert = mouseY / height - 0.5;
+
+    x.set(xPert);
+    y.set(yPert);
+  };
+
+  const handelMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  };
+  return (
+    <>
+      <motion.div
+        style={{
+          transformStyle: "preserve-3d",
+          rotateX,
+          rotateY,
+        }}
+        onMouseMove={handelMouseMove}
+        onMouseLeave={handelMouseLeave}
+        className="absolute p-10  rounded-lg bg-gradient-to-br from-indigo-300 to-violet-200 shadow-2xl"
+      >
+        <motion.img
+          style={{
+            transform: "translateZ(60px)",
+            transformStyle: "preserve-3d",
+          }}
+          className="relative rounded-lg shadow-3xl"
+          src={`${img}`}
+          width={`${width}`}
+        />
+      </motion.div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
 "use client";
 import React from "react";
-import ThreeCardScroll from "./components/card/ThreeCardScroll";
-import DarkBgGrid from "./components/Background/Dark-Bg-grid";
-import ThreeCardHover from "./components/card/ThreeCardHover";
-import SlideCard from "./components/card/Modal-Card";
-import HoverTiltImage from "./components/Images/Hover-Tap-Image";
-import DragImage from "./components/Images/Darg-Images";
-import HoverTapImage from "./components/Images/Hover-Tap-Image";
-import ModalCard from "./components/card/Modal-Card";
+import HoverTiltImage from "./components/3D Effect/Hover-Tilt-Image";
 
 const img1 =
   "https://img.freepik.com/free-photo/anime-japanese-character_23-2151478190.jpg?ga=GA1.1.1804695545.1747980337&semt=ais_hybrid&w=740";
@@ -22,7 +15,7 @@ export default function Home() {
   return (
     <>
       <div className="flex justify-center mt-200 mb-300">
-        <ModalCard img={img3} width={200} />
+        <HoverTiltImage img={img3} width={400} />
       </div>
     </>
   );
